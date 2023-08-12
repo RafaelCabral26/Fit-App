bc4fdefea81edd5c452ffc1559b3a0399bf26799
RafaelCabral26 <rafaelcabral261992@gmail.com>
2023-08-12 18:38:27 -0300

    adding:config-dockerr

---
+++ fit-app/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,12 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/app/src/app/layout.tsx": [
+      "/app/src/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/app/src/app/page": [
+      "/app/src/app/globals.css"
+    ]
+  }
+}

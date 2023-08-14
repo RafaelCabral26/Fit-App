4fad56960741571a90ebf4825b5e3fd0907f5790
RafaelCabral26 <rafaelcabral261992@gmail.com>
2023-08-13 21:25:51 -0300

    add:docker-compose

--- fit-app/.next/server/flight-server-css-manifest.js
+++ fit-app/.next/server/flight-server-css-manifest.js
@@ -1,12 +1,4 @@
 self.__RSC_CSS_MANIFEST={
-  "cssImports": {
-    "/app/src/app/layout.tsx": [
-      "/app/src/app/globals.css"
-    ]
-  },
-  "cssModules": {
-    "/app/src/app/page": [
-      "/app/src/app/globals.css"
-    ]
-  }
+  "cssImports": {},
+  "cssModules": {}
 }

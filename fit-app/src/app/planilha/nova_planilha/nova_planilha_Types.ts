93f96e7d3e67fb8c489a211ff71f961696e9dace
RafaelCabral26 <rafaelcabral261992@gmail.com>
2023-08-31 23:13:43 -0300

    ongoing:add exercise modal

--- fit-app/src/app/planilha/nova_planilha/nova_planilha_Types.ts
+++ fit-app/src/app/planilha/nova_planilha/nova_planilha_Types.ts
@@ -14,21 +14,21 @@ export type TDays = {
 }
 export type TMuscleGroups = TArm | TBack | TChest | TLegs | TShoulder;
 type TArm = {
-    muscle_group?:"Bracos" | "arm",
+    muscle_group?:"Bracos" | "arm" | "",
     subgroup?:"Biceps" | "Triceps" | "Antebraço" | "",
 }
 type TBack ={
-    muscle_group?:"Costas" | "back",
+    muscle_group?:"Costas" | "back" | "",
     subgroup?: "Superior" | "Dorsal" | "Inferior" | "";}
 type TChest ={
-    muscle_group?:"Peito" | "chest" ,
+    muscle_group?:"Peito" | "chest" | "" ,
     subgroup?:  "Superior" | "Medial" | "Inferior" | ""
 }
 type TLegs ={
-    muscle_group?:"Pernas" | "leg",
+    muscle_group?:"Pernas" | "leg"| "",
     subgroup?: "Posterior" | "Gluteos" | "Quadriceps" | "Panturrilha" | ""
 }
 type TShoulder = {
-    muscle_group?:"Ombros" | "shoulder",
+    muscle_group?:"Ombros" | "shoulder" | "",
     subgroup?: "Anterior" | "Posterior" | "Lateral" | "",
 }

df38fcf16e5737b363c259974f3ffaf2f2dec922
RafaelCabral26 <rafaelcabral261992@gmail.com>
2023-09-10 18:34:51 -0300

    ongoing:select subgroup filter

--- fit-app/src/app/planilha/nova_planilha/nova_planilha_Types.ts
+++ fit-app/src/app/planilha/nova_planilha/nova_planilha_Types.ts
@@ -5,30 +5,31 @@ export type TExercise = {
     quantity: number,
     uId: string,
     createdAt?: string,
-} & (TMuscleGroups) 
+} & (TMuscleGroupsObj) 
 
 export type TPossibleDays = "day1" | "day2" | "day3" | "day4" | "day5" | "day6" | "day7"
 export type TDays = {
     day: TPossibleDays,
     exercises: TExercise[] | []
 }
-export type TMuscleGroups = TArm | TBack | TChest | TLegs | TShoulder;
-type TArm = {
-    muscle_group?:"Bracos" | "arm" | "",
+export type TMuscleGroupsObj = TArm | TBack | TChest | TLegs | TShoulder;
+export type TMuscleGroups = "Braços" | "Costas" | "Peitoral" | "Pernas" | "Ombros";
+export type TArm = {
+    muscle_group?:"Braços" | "",
     subgroup?:"Biceps" | "Triceps" | "Antebraço" | "",
 }
-type TBack ={
-    muscle_group?:"Costas" | "back" | "",
+export type TBack ={
+    muscle_group?:"Costas" | "",
     subgroup?: "Superior" | "Dorsal" | "Inferior" | "";}
-type TChest ={
-    muscle_group?:"Peito" | "chest" | "" ,
+export type TChest ={
+    muscle_group?:"Peitoral" | "" ,
     subgroup?:  "Superior" | "Medial" | "Inferior" | ""
 }
-type TLegs ={
-    muscle_group?:"Pernas" | "leg"| "",
+export type TLegs ={
+    muscle_group?:"Pernas" | "",
     subgroup?: "Posterior" | "Gluteos" | "Quadriceps" | "Panturrilha" | ""
 }
-type TShoulder = {
-    muscle_group?:"Ombros" | "shoulder" | "",
+export type TShoulder = {
+    muscle_group?:"Ombros" | "",
     subgroup?: "Anterior" | "Posterior" | "Lateral" | "",
 }

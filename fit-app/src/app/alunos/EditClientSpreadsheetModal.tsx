25fa9d82352a5fa9de3e5a3a8e292f735961d395
RafaelCabral26 <rafaelcabral261992@gmail.com>
2023-10-09 19:13:47 -0300

    add:delete specific client spreadsheet UI/API

--- fit-app/src/app/alunos/EditClientSpreadsheetModal.tsx
+++ fit-app/src/app/alunos/EditClientSpreadsheetModal.tsx
@@ -1,27 +1,31 @@
 import createQueryString from "@/services/createQueryString";
 import { useRouter } from "next/navigation";
-import { SetStateAction, useState } from "react";
+import { useContext, SetStateAction, useState } from "react";
 import { formatDate } from "../planilha/construtor_planilha/Spreadsheet_Utilities";
 import myHTTP from "@/services/axiosconfig";
+import { GlobalContext } from "@/services/MyToast"
 
 const EditClientSpreadsheetModal = ({ selectedClient, showEditModal }: { selectedClient: any[] | null, showEditModal: React.Dispatch<SetStateAction<boolean>> }) => {
     const router = useRouter();
     const [selectedSpreadsheet, setSelectedSpreadsheet] = useState<string | null>(null);
+    const globalState = useContext(GlobalContext);
     const handleSelected = (event: React.ChangeEvent<HTMLSelectElement>) => {
         setSelectedSpreadsheet(event.target.value);
-    }
+    };
     
     const redirectToEditSpreadsheet = () => {
         router.replace(`/planilha/construtor_planilha?${createQueryString("spreadsheet_id", selectedSpreadsheet)}&${createQueryString("previous_url","alunos")}`);
-    }
+    };
+
     const handleDeleteClientSpreadsheet = (clientSpreadsheetId:string | null) => {
         if (clientSpreadsheetId === null) return;
         myHTTP.delete(`delete_client_spreadsheet/${clientSpreadsheetId}`)
             .then(res => {
-                console.log(res);
+                globalState?.setToast({type:"success", message:res.data.msg});
+                showEditModal(false);
             })
         .catch(err => {
-                console.log(err);
+                globalState?.setToast({type:"warning", message:err.response.data.msg});
             });
     };
     

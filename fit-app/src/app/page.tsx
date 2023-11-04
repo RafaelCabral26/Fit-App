bafa041e47f33e3bc4dba24433b5924fd71a5bd9
RafaelCabral26 <rafaelcabral261992@gmail.com>
2023-11-04 20:27:17 -0300

    finished landing page

--- fit-app/src/app/page.tsx
+++ fit-app/src/app/page.tsx
@@ -7,8 +7,8 @@ export default function MyApp() {
         <div>
             <div className='absolute top-0 right-0 z-0 scale-x-[-1]  w-full h-4/5 lg:h-[95%] 2xl:h-full  bg-no-repeat bg-cover bg-center bg-[url("../../public/teste-6.JPG")]' >
             </div>
-            <section className="relative  grid xl:gap-y-12 2xl:gap-y-20 text-white w-full md:h-[80vh] grid-cols-12 grid-rows-6">
-                <div className="place-self-center  col-start-2 xl:col-start-2 2xl:col-start-2 col-span-12 xl:col-span-8 2xl:col-span-6  row-start-2 2xl:row-start-3  ">
+            <section className="relative h-[75vh] lg:h-[85vh] 2xl:h-[95vh] grid  xl:gap-y-12 2xl:gap-y-20 text-white w-full  grid-cols-12 grid-rows-6 border-2 border-secondary">
+                <div className="place-self-center col-start-2 xl:col-start-2 2xl:col-start-2 col-span-12 xl:col-span-8 2xl:col-span-6  row-start-2 xl:row-start-3  ">
                     <span className="my-landing-text">Cuide de seu
                         <span className="my-landing-text text-secondary mx-4"> Corpo</span>
                     </span>
@@ -16,7 +16,7 @@ export default function MyApp() {
                         <span className="my-landing-text text-secondary mx-4"> Rotina</span>
                     </span>
                 </div>
-                <div className="col-start-2 md:col-start-2 xl:col-start-2 row-start-3 2xl:row-start-4 col-span-9 md:col-span-6 xl:col-span-4 ">
+                <div className="col-start-2 md:col-start-2 xl:col-start-2 row-start-3 xl:row-start-4 col-span-9 md:col-span-6 xl:col-span-4 ">
                     <span className="text-white 2xl:text-2xl ">
                         Conheça as melhores ferramentas de monitoramento de rotina,  progresso e saúde. Acompanhe o
                         seu progresso e de seus clientes de
@@ -24,11 +24,11 @@ export default function MyApp() {
                         <br />
                     </span>
                 </div>
-                <div className="place-self-center justify-self-start md:place-self-start col-start-2  col-span-6  row-start-4 2xl:row-start-5 ">
+                <div className="place-self-end justify-self-start md:place-self-start col-start-2  col-span-6  row-start-4 xl:row-start-5 ">
                     <button className="btn btn-secondary xl:btn-wide rounded-sm">Saiba Mais</button>
                 </div>
             </section>
-            <section className="container h-[90vh] m-auto border-2 border-red-500">
+            <section className="container my-10 h-screen m-auto border-2 border-red-500">
                 Teste
             </section>
         </div>

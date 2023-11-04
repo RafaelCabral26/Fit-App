cad6d775c8f7504cf64e9fc7945d37eec36cde0f
RafaelCabral26 <rafaelcabral261992@gmail.com>
2023-11-04 19:56:14 -0300

    finished landing page

--- fit-app/src/app/planilha/construtor_planilha/DayComponent.tsx
+++ fit-app/src/app/planilha/construtor_planilha/DayComponent.tsx
@@ -5,6 +5,7 @@ import AddExerciseFormModal from "./modalAddNewExercise"
 import { TDays, TExercise } from "./Spreadsheet_Types";
 import { GlobalContext } from "@/services/GlobalContext";
 import { useOnClickOutside } from "@/services/ClickOutsideHook";
+import DragIndicatorSvg from "@/svgs/dragIndicator";
 
 const DayComponent = ({ index, day, daysArray, setNewDayArray }: { index: number, day: TDays, daysArray: TDays[], setNewDayArray: React.Dispatch<SetStateAction<TDays[]>> }) => {
     const globalState = useContext(GlobalContext);
@@ -31,15 +32,18 @@ const DayComponent = ({ index, day, daysArray, setNewDayArray }: { index: number
             <Droppable type="droppableExercise" key={day.dayUID} droppableId={`${day.dayUID}`}>
                 {(provided, snapshot) => {
                     return (
-                        <div className={`my-day-container ${snapshot.isDraggingOver ? "bg-primary bg-opacity-80" : "bg-base-200"}`}
+                        <div className={`my-day-container ${snapshot.isDraggingOver ? "bg-secondary bg-opacity-70" : "bg-base-200"}`}
                             {...provided.droppableProps} ref={provided.innerRef}>
-                            <div className="flex justify-between p-2 bg-base-300 rounded-t-md">
-                                <h2>{"Dia " + (index + 1)}</h2>
-                                <button onClick={() => showOptions(!optionsDropdown)} className="relative text-2xl ">...
+                            <div className="grid grid-cols-3 grid-rows-2 p-2 bg-primary text-white rounded-t-sm h-12">
+                                <h2 className="font-mono row-start-2 leading-4">{"Dia " + (index + 1)}</h2>
+                                <div className="w-[20px]  text-white/40 row-start-1  col-start-2 justify-self-center">
+                                    <DragIndicatorSvg></DragIndicatorSvg>
+                                </div>
+                                <button onClick={() => showOptions(!optionsDropdown)} className="relative row-start-2 col-start-3 justify-self-end text-2xl leading-4  ">...
                                     {optionsDropdown &&
                                         <div ref={clickRef} className="absolute right-0">
-                                            <ul tabIndex={0} className="my-dropdown">
-                                                <li onClick={handleModal}><a>Adicionar</a></li>
+                                            <ul tabIndex={0} className="my-dropdown ">
+                                                <li className="text-primary" onClick={handleModal}><a>Adicionar</a></li>
                                                 <li onClick={handleDeleteDay} className="text-red-500 hover:text-red-400"><a>Deletar</a></li>
                                             </ul>
                                         </div>

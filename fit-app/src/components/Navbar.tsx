78e1af64efceb5d0d58fb52f6c91f368a9e4ccf0
RafaelCabral26 <rafaelcabral261992@gmail.com>
2023-11-06 19:25:51 -0300

    added some section 2 layout

--- fit-app/src/components/Navbar.tsx
+++ fit-app/src/components/Navbar.tsx
@@ -55,7 +55,7 @@ export const Navbar = () => {
 
     return (
         <>
-            <div className={`navbar relative z-30 justify-center md:justify-between px-[15vw] 2xl:px-[18vw] p-4 ${myUrl !== "/" ? "bg-primary" : "bg-gray-200/20"}  `}>
+            <div className={`navbar relative z-30 justify-center md:justify-between px-[15vw] 2xl:px-[18vw] p-4 ${myUrl !== "/" ? "bg-base-200" : "bg-gray-200/20"}  `}>
                 <a href="/" className='w-48 md:w-56'>
                     <FitAndAppLogo></FitAndAppLogo>
                 </a>

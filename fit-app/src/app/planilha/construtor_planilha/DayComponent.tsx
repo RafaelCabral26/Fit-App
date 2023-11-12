720ad068934835abe56530edf11986c1dc970253
RafaelCabral26 <rafaelcabral261992@gmail.com>
2023-11-12 16:02:05 -0300

    add confirm delete day modal

--- fit-app/src/app/planilha/construtor_planilha/DayComponent.tsx
+++ fit-app/src/app/planilha/construtor_planilha/DayComponent.tsx
@@ -10,6 +10,7 @@ import DragIndicatorSvg from "@/svgs/dragIndicator";
 const DayComponent = ({ index, day, daysArray, setNewDayArray }: { index: number, day: TDays, daysArray: TDays[], setNewDayArray: React.Dispatch<SetStateAction<TDays[]>> }) => {
     const globalState = useContext(GlobalContext);
     const [optionsDropdown, showOptions] = useState(false);
+    const [deleteDayModal, showDeleteDayModal] = useState<boolean>(false);
     const [newExerciseModal, showNewExerciseModal] = useState(false);
     const clickRef = useRef(null);
     useOnClickOutside(clickRef, () => {
@@ -27,6 +28,7 @@ const DayComponent = ({ index, day, daysArray, setNewDayArray }: { index: number
         showNewExerciseModal(true);
         globalState?.isDragDisabledSwitch(true);
     }
+
     return (
         <>
             <Droppable direction={window.innerWidth > 640 ? "horizontal" : "vertical"}
@@ -45,22 +47,41 @@ const DayComponent = ({ index, day, daysArray, setNewDayArray }: { index: number
                                         <div ref={clickRef} className="absolute right-0 md:right-auto md:bottom-0 md:left-4 ">
                                             <ul tabIndex={0} className="my-dropdown  ">
                                                 <li className="text-primary" onClick={handleModal}><a>Adicionar</a></li>
-                                                <li onClick={handleDeleteDay} className="text-red-500 hover:text-red-400"><a>Deletar</a></li>
+                                                <li onClick={() => showDeleteDayModal(true)} className="text-red-500 hover:text-red-400"><a>Deletar</a></li>
                                             </ul>
                                         </div>
                                     }
+                                    {
+                                        deleteDayModal &&
+                                        (
+                                            <div className="bg-white text-neutral flex flex-col items-center text-lg justify-between gap-4 p-10 cursor-auto border-2  rounded-md fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
+                                                <div className="flex flex-col">
+                                                    <span>Deseja deletar</span>
+                                                    <span className="font-bold uppercase">{"Dia " + (index + 1)+ "?"}</span>
+                                                </div>
+                                                <div className="flex gap-2 ">
+                                                    <button onClick={handleDeleteDay} className="btn btn-md rounded-sm btn-secondary ">
+                                                        deletar
+                                                    </button>
+                                                    <button onClick={() => { showDeleteDayModal(false) }} className="btn btn-md rounded-sm text-white btn-warning">
+                                                        cancelar
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        )
+                                    }
                                 </button>
+                            </div >
+                            <div className="flex flex-col sm:flex-row">
+                                {day.exercises.map((e: TExercise, index: number) => {
+                                    return <ExerciseComponent daysArray={daysArray} setNewDayArray={setNewDayArray} dayIndex={dayIndex} key={e.uId} item={e} index={index}></ExerciseComponent>
+                                })}
+                                {provided.placeholder}
                             </div>
-                        <div className="flex flex-col sm:flex-row">
-                            {day.exercises.map((e: TExercise, index: number) => {
-                                return <ExerciseComponent daysArray={daysArray} setNewDayArray={setNewDayArray} dayIndex={dayIndex} key={e.uId} item={e} index={index}></ExerciseComponent>
-                            })}
-                            {provided.placeholder}
-                            </div>
-                        </div>
+                        </div >
                     )
                 }}
-            </Droppable>
+            </Droppable >
             {newExerciseModal &&
                 <AddExerciseFormModal showNewExerciseModal={showNewExerciseModal} dayObject={day.exercises} daysArray={daysArray}></AddExerciseFormModal>
             }

a7ccbd00a30dac9fcb4ba3c4aecf96fff220b478
RafaelCabral26 <rafaelcabral261992@gmail.com>
2023-11-28 18:56:49 -0300

    API port change

--- fit-api/build/routes/routes.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const allRoutes: import("express-serve-static-core").Router;
--- fit-api/build/services/AppError.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class AppError<T extends string> extends Error {
-    statusCode: number;
-    message: string;
-    constructor(statusCode: number, message: string);
-}

68876061c3020184fa3e0fa8ea3ac62f267945a8
RafaelCabral26 <rafaelcabral261992@gmail.com>
2023-12-05 17:41:15 -0300

    nginx teste

--- db/init.sql
+++ db/init.sql
@@ -58,292 +58,292 @@ DELIMITER ;
 INSERT INTO exercises_samples(exercise_id,exercise_name, muscle_group, subgroup)
 VALUES 
     (
-         
+         uuid_v4(),
          "Flexão",
          "Peitoral",
          "Medial"
     ),
     (
-		 
+		 uuid_v4(),
          "Flexão Apoiado",
          "Peitoral",
          "Medial"
     ),
     (
-		 
+		 uuid_v4(),
         "Flexão de Joelho",
          "Peitoral",
          "Medial"
     ),
     (
-		 
+		 uuid_v4(),
          "Supino Reto Barra",
          "Peitoral",
          "Medial"
     ),
     (
-		 
+		 uuid_v4(),
          "Supino Inclinado Barra",
          "Peitoral",
          "Superior"
     ),
     (
-		 
+		 uuid_v4(),
          "Supino Declinado Barra",
          "Peitoral",
          "Inferior"
     ),
     (
-		 
+		 uuid_v4(),
          "Supino Reto Halter",
          "Peitoral",
          "Medial"
     ),
     (
-		 
+		 uuid_v4(),
          "Supino Halter 45",
          "Peitoral",
          "Superior"
     ),
     (
-		 
+		 uuid_v4(),
          "Supino Halter Declinado",
          "Peitoral",
          "Inferior"
     ),
     (
-		 
+		 uuid_v4(),
          "Supino Reto Máquina",
          "Peitoral",
          "Medial"
     ),
     (
-		 
+		 uuid_v4(),
          "Supino 45 Máquina",
          "Peitoral",
          "Superior"
     ),
     (
-		 
+		 uuid_v4(),
          "Supino Declinado Máquina",
          "Peitoral",
          "Inferior"
     ),
     (
-		 
+		 uuid_v4(),
          "Crucifixo Reto",
          "Peitoral",
          "Medial"
     ),
     (
-		 
+		 uuid_v4(),
          "Crucifixo Inclinado",
          "Peitoral",
          "Superior"
     ),
     (
-		 
+		 uuid_v4(),
 		 "Crucifixo Voador",
          "Peitoral",
          "Medial"
     ),
     (
-		 
+		 uuid_v4(),
          "Crucifixo Inclinado Máquina",
          "Peitoral",
          "Superior"
     ),
     (
-		 
+		 uuid_v4(),
         "Pullover",
          "Peitoral",
         "Medial"
     ),
     (
-		 
+		 uuid_v4(),
          "Remada Barra",
          "Costas",
          "Dorsal"
     ),
     (
-		 
+		 uuid_v4(),
          "Remada Halter",
          "Costas",
          "Dorsal"
     ),
     (
-		 
+		 uuid_v4(),
          "Remada Máquina",
          "Costas",
          "Dorsal"
     ),
     (
-		 
+		 uuid_v4(),
          "Puxada Supinada",
          "Costas",
          "Dorsal"
     ),
     (
-		 
+		 uuid_v4(),
          "Puxada Pronada",
          "Costas",
          "Dorsal"
     ),
     (
-		 
+		 uuid_v4(),
          "Puxada Neutra",
          "Costas",
          "Dorsal"
     ),
     (
-		 
+		 uuid_v4(),
          "Barra Supinada",
          "Costas",
          "Dorsal"
     ),
     (
-		 
+		 uuid_v4(),
          "Barra Pronada",
          "Costas",
          "Dorsal"
     ),
     (
-		 
+		 uuid_v4(),
          "Barra Neutra",
          "Costas",
          "Dorsal"
     ),
     (
-		 
+		 uuid_v4(),
          "Pull Down Polia",
         "Costas",
         "Dorsal"
     ),
     (
-		 
+		 uuid_v4(),
          "Hiperextensao Lombar",
          "Costas",
         "Inferior"
     ),
     (
-		 
+		 uuid_v4(),
         "Levantamento Terra",
          "Costas",
          NULL
     ),
     (
-		 
+		 uuid_v4(),
         "Remada Alta",
         "Ombros",
         NULL
     ),
     (
-		 
+		 uuid_v4(),
         "Desenvolvimento Lateral Halter",
         "Ombros",
         "Lateral"
     ),
     (
-		 
+		 uuid_v4(),
         "Desenvolvimento Frontal Halter",
         "Ombros",
         "Anterior"
     ),
     (
-		 
+		 uuid_v4(),
         "Desenvolvimento Lateral Máquina",
         "Ombros",
         "Lateral"
     ),
     (
-		 
+		 uuid_v4(),
         "Desenvolvimento Frontal Máquina",
         "Ombros",
         "Anterior"
     ),
     (
-		 
+		 uuid_v4(),
         "Elevação Lateral Halter",
          "Ombros",
         "Lateral"
     ),
     (
-		 
+		 uuid_v4(),
         "Elevação Frontal Halter",
          "Ombros",
         "Anterior"
     ),
     (
-		 
+		 uuid_v4(),
         "Elevação Lateral Polia",
          "Ombros",
         "Lateral"
     ),
     (
-		 
+		 uuid_v4(),
         "Elevação Frontal Polia",
          "Ombros",
         "Anterior"
     ),
     (
-		 
+		 uuid_v4(),
          "Crucifixo Invertido Halter",
          "Ombros",
         "Posterior"
     ),
     (
-		 
+		 uuid_v4(),
          "Crucifixo Invertido Máquina",
          "Ombros",
         "Posterior"
     ),
     (
-		 
+		 uuid_v4(),
         "Tríceps Polia",
         "Braços",
         "Tríceps"
     ),
     (
-		 
+		 uuid_v4(),
         "Tríceps Invertido Polia",
         "Braços",
         "Tríceps"
     ),
     (
-		 
+		 uuid_v4(),
         "Tríceps Coice Polia",
         "Braços",
         "Tríceps"
     ),
     (
-		 
+		 uuid_v4(),
         "Tríceps Corda Polia",
         "Braços",
         "Tríceps"
     ),
     (
-		 
+		 uuid_v4(),
         "Tríceps Testa Polia",
         "Braços",
         "Tríceps"
     ),
     (
-		 
+		 uuid_v4(),
         "Tríceps Coice Halter",
         "Braços",
         "Tríceps"
     ),
     (
-		 
+		 uuid_v4(),
         "Tríceps Testa Halter",
         "Braços",
         "Tríceps"
     ),
     (
-		 
+		 uuid_v4(),
         "Tríceps Banco",
         "Braços",
         "Tríceps"
     ),
-    ("Paralela","Braços","Tríceps");
+    (uuid_v4(),"Paralela","Braços","Tríceps");
     
--- fit-app/src/services/axiosconfig.tsx
+++ fit-app/src/services/axiosconfig.tsx
@@ -1,11 +1,11 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 const config: AxiosRequestConfig = {
-    baseURL: "https://fit-api-fduu.onrender.com:10000/",
+    baseURL: "http://localhost:6868/",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json;charset=utf-8",
-        "Access-Control-Allow-Origin": "http://localhost:8080/",
+        "Access-Control-Allow-Origin": "http://localhost:6868/",
     }
 }
 const myHTTP = axios.create(config);

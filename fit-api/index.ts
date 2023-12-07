6bac8a20962a9fec7b1e27965752bb16942a45e9
RafaelCabral26 <rafaelcabral261992@gmail.com>
2023-12-07 20:40:44 -0300

    api multiple origin cors

--- fit-api/index.ts
+++ fit-api/index.ts
@@ -1,6 +1,5 @@
 import dotenv from "dotenv"
 dotenv.config();
-import { Error } from "sequelize";
 import errorHandler from "./middleware/errorHandler";
 import { allRoutes } from "./routes/routes";
 const sequelize = require("./models/createSequelize");
@@ -8,10 +7,17 @@ const express = require("express");
 const cors = require('cors');
 const cookieParser = require("cookie-parser")
 const app = express();
-console.log("TESTEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE",process.env.CORS_ORIGIN);
+console.log("TESTEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE", process.env.CORS_ORIGIN);
 
+const multipleOrigins = ["http://191.252.210.147/", "http://cabral.vps-kinghost.net/"];
 const corsOptions = {
-    origin:"http://localhost",
+    origin: function(origin:string, callback:any) {
+        if (multipleOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error("Erro na origem"),false);
+        }
+    },
     credentials: true,
 }
 app.use(cors(corsOptions));

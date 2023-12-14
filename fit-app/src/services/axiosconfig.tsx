fb22f1b6693da474f41c44630d34979d436da40f
root <root@cabral.vps-kinghost.net>
2023-12-14 10:56:48 -0300

    added nginx ssl to app/api

--- fit-app/src/services/axiosconfig.tsx
+++ fit-app/src/services/axiosconfig.tsx
@@ -1,11 +1,11 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 const config: AxiosRequestConfig = {
-    baseURL: "http://191.252.210.147:6868/",
+    baseURL: "https://fitandapp.site/api/",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json;charset=utf-8",
-        "Access-Control-Allow-Origin": "http://191.252.210.147:6868/",
+        "Access-Control-Allow-Origin": "https://fitandapp.site",
     }
 }
 const myHTTP = axios.create(config);
